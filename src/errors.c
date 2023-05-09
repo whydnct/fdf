d505fe3b0faf6b92c622222e501e3a01417f55b2
whydnct <82403599+whydnct@users.noreply.github.com>
2023-05-09 20:14:35 +0000

    .

--- src/errors.c
+++ src/errors.c
@@ -1,35 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_fdf.h                                           :+:      :+:    :+:   */
+/*   errors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aperez-m <aperez-m@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/28 18:30:39 by aperez-m          #+#    #+#             */
-/*   Updated: 2023/05/09 13:25:03 by aperez-m         ###   ########.fr       */
+/*   Created: 2023/05/09 13:11:41 by aperez-m          #+#    #+#             */
+/*   Updated: 2023/05/09 13:22:34 by aperez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_FDF_H
-# define FT_FDF_H
+#include "ft_fdf.h"
 
-# define EMPTY_FILE 1
-
-# include "mlx.h"
-# include "ft.h"
-# include <stdio.h>
-# include <math.h>
-
-void	error_handler(int error);
-
-typedef struct s_data{
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-
-#endif
-
-
+void    error_handler(int error)
+{
+    if (error == 1)
+        ft_putstr_fd("File is empty", 2);
+    exit(error);
+}
